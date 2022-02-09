f60aac0383bf822f1398e42625c16dfd49110d86
seungrodotlee <srlee0226w@gmail.com>
2022-02-09 17:14:31 +0900

    .

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,18 +3,9 @@ import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { toggleTheme, setTheme } from "../store/modules/theme";
 
 const Home: React.FC = () => {
-  const test = useAppSelector((state) => state.theme.theme);
-  const dispatch = useAppDispatch();
-
   return (
     <div className="w-full h-full flex-center flex-col">
-      My React App {test}
-      <div className="mt-2">
-        <button className="mr-4" onClick={() => dispatch(setTheme("light"))}>
-          light
-        </button>
-        <button onClick={() => dispatch(setTheme("dark"))}>dark</button>
-      </div>
+      <div className="dot bg-foreground w-8 h-8 rounded-full"></div>
     </div>
   );
 };
