2cabf9ac91c1ee1f89a1bf74e98ab05b403e9239
seungrodotlee <srlee0226w@gmail.com>
2022-02-10 18:23:52 +0900

    [22.02.10:18:23] 인트로 애니메이션 작업중

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
+import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { toggleTheme, setTheme } from "../store/modules/theme";
 
 const Home: React.FC = () => {
+  const { sqrt, pow, ceil } = Math;
+
+  const diag = sqrt(pow(window.innerHeight, 2) + pow(window.innerWidth, 2));
+  console.log("diag?", diag);
+  const dotSize = ceil(diag / (16 * 13)) * 13;
+  const ratio = 1 / ceil(dotSize / 13);
+  console.log(dotSize, ratio);
+  const [dotStyle, setDotStyle] = useState({
+    width: `${dotSize}rem`,
+    height: `${dotSize}rem`,
+    transform: `scale(1)`,
+  });
+
   return (
-    <div className="w-full h-full flex-center flex-col">
-      <div className="dot bg-foreground w-8 h-8 rounded-full"></div>
+    <div className="w-full h-full flex-center flex-col overflow-hidden">
+      <div className="flex items-center">
+        <div className="dot bg-foreground rounded-full" style={dotStyle}></div>
+        {/* <p className="font-black text-[16rem] ml-2">log</p> */}
+      </div>
     </div>
   );
 };
