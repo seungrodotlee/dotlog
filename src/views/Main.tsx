fbc505bad239bef2ba54afc602de475918733ec7
seungrodotlee <srlee0226w@gmail.com>
2022-02-14 15:12:40 +0900

    [22.02.14:15:12] 메인 노이즈

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -4,36 +4,57 @@ import styled from "styled-components";
 
 import noise from "../assets/img/noise.svg";
 
-const About: FC = () => {
-  const Noise = styled.div`
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(to bottom, #424242, transparent), url(${noise});
-    filter: contrast(170%) brightness(1000%);
+import colors from "tailwindcss/colors";
 
-    @media all and (-webkit-min-device-pixel-ratio: 0) and (min-resolution: 0.001dpcm) {
-      .noise {
-        filter: contrast(290%) brightness(1000%);
-      }
+const Noise = styled.div`
+  position: absolute;
+  top: -50%;
+  left: -50%;
+  width: 200%;
+  height: 200%;
+  background: url(${noise}) 0 0 repeat;
+  background-position: center;
+
+  animation: bg-animation 1.5s steps(6) infinite;
+
+  @keyframes bg-animation {
+    0%,
+    100% {
+      transform: translate(0, 0);
     }
-  `;
+    10% {
+      transform: translate(-5%, -10%);
+    }
+    30% {
+      transform: translate(3%, -15%);
+    }
+    50% {
+      transform: translate(12%, 9%);
+    }
+    70% {
+      transform: translate(9%, 4%);
+    }
+    90% {
+      transform: translate(-1%, 7%);
+    }
+  }
+`;
 
-  const Overlay = styled.div`
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    mix-blend-mode: multiply;
-  `;
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  mix-blend-mode: multiply;
+`;
 
+const About: FC = () => {
   return (
     <div className="w-full h-full flex-col">
       <div className="absolute w-full h-48 bg-gradient-to-b from-black to-transparent"></div>
-      <div className="container relative !px-0 mt-48 bg-gradient-to-b from-neutral-900 rounded-3xl">
+      <div className="container relative !px-0 mt-48 rounded-3xl overflow-hidden mb-36">
         <Noise />
-        <Overlay className="bg-neutral-800"></Overlay>
+        <Overlay className="bg-gradient-to-b from-neutral-700 to-neutral-400"></Overlay>
         <div className="w-full h-screen"></div>
         <div className="w-full h-screen"></div>
       </div>
