633621309afccd087f88a8d5ae5709380df378d5
seungrodotlee <srlee0226w@gmail.com>
2022-02-23 16:36:23 +0900

    [22.02.23:16:35] 폴더링을 통한 서브 카테고리 적용되도록 구현

--- articleExporter.js
+++ articleExporter.js
@@ -24,31 +24,74 @@ watcher.on("add", () => {
   }, 100);
 });
 
+console.log("article exporter is now running...");
+console.log(
+  "since now, we will automatically modify './src/article/index.tsx' when you create or delete, rename articles under './src/articles/SOME-FOLDERS'"
+);
+
 const generate = async () => {
   let importSection = "";
-  let exportSection = "export default {\n";
+  let exportSection = "const articles = {\n";
 
-  const categoryList = await fs.promises.readdir(articleFolder);
+  const scanDirectory = (root, name, depth) => {
+    return new Promise(async (res, rej) => {
+      const folder = `${root}/${name}`;
 
-  const categories = categoryList.filter((c) => !c.includes("."));
+      const list = await fs.promises.readdir(folder);
 
-  for (let c of categories) {
-    exportSection += `  ${c}: {\n`;
-    const articleList = await fs.promises.readdir(`${articleFolder}/${c}`);
+      const subFolders = list.filter((l) => !l.includes("."));
+      const articles = list.filter((l) => l.includes(".md"));
 
-    const articles = articleList.filter((a) => a.includes(".md"));
+      if (depth > 0) {
+        for (let i = 0; i < depth * 2; i++) {
+          exportSection += " ";
+        }
 
-    articles.forEach((a) => {
-      const title = a.replace(".md", "");
+        exportSection += `${name}: {\n`;
+      }
 
-      importSection += `import ${title} from "./${c}/${a}"\n`;
-      exportSection += `    ${title},\n`;
+      if (subFolders) {
+        for (let idx in subFolders) {
+          const f = subFolders[idx];
+
+          await scanDirectory(folder, f, depth + 1);
+        }
+      }
+
+      if (articles) {
+        articles.forEach((a) => {
+          const title = a.replace(".md", "");
+
+          importSection += `import ${title} from "${folder.replace(
+            "./src/articles",
+            "."
+          )}/${a}";\n`;
+
+          for (let i = 0; i < (depth + 1) * 2; i++) {
+            exportSection += " ";
+          }
+
+          exportSection += `${title},\n`;
+        });
+      }
+
+      for (let i = 0; i < depth * 2; i++) {
+        exportSection += " ";
+      }
+
+      if (depth > 0) {
+        exportSection += `},\n`;
+      }
+
+      res();
     });
-    exportSection += "  },\n";
-  }
-  exportSection += "}";
+  };
 
-  const code = `${importSection}\n${exportSection}`;
+  await scanDirectory("./src", "articles", 0);
+
+  exportSection += "};";
+
+  const code = `${importSection}\n${exportSection}\n\nexport default articles;`;
 
   fs.writeFile("./src/articles/index.tsx", code, "utf8", (err) => {
     if (err) {
--- src/articles/index.tsx
+++ src/articles/index.tsx
@@ -1,10 +1,14 @@
-import beginning from "./dotlog/beginning.md"
-import about from "./react/about.md"
-import base from "./react/base.md"
-import jsx from "./react/jsx.md"
+import file2 from "./dotlog/somefolder/file2.md";
+import beginning from "./dotlog/beginning.md";
+import about from "./react/about.md";
+import base from "./react/base.md";
+import jsx from "./react/jsx.md";
 
-export default {
+const articles = {
   dotlog: {
+    somefolder: {
+      file2,
+    },
     beginning,
   },
   react: {
@@ -12,4 +16,6 @@ export default {
     base,
     jsx,
   },
-}
+};
+
+export default articles;
