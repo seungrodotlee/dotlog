def017edf952f6e85006bcacaf7b81fa46ce4695
seungrodotlee <srlee0226w@gmail.com>
2022-02-23 16:37:09 +0900

    [22.02.23:16:36] redux article 모듈 서브 카테고리 대응 및 리듀서를 사용한 코드 개선

--- src/store/modules/article.tsx
+++ src/store/modules/article.tsx
@@ -27,23 +27,70 @@ export const fetchArticleList = createAsyncThunk<
 
   if (article) return article;
 
-  const result = {};
+  // const result = {};
 
-  for (let k in ArticleList) {
-    const data = ArticleList[k];
-    result[k] = {};
+  // for (let k in ArticleList) {
+  //   const data = ArticleList[k];
+  //   result[k] = {};
 
-    for (let key in data) {
-      const d = data[key];
+  //   for (let key in data) {
+  //     const d = data[key];
+  //     console.log("d", d);
 
+  //     const fetched = await fetch(d);
+  //     const text = await fetched.text();
+
+  //     const html: string = markdownConverter.makeHtml(text);
+
+  //     result[k][key] = html;
+  //   }
+  // }
+
+  const callback = async (acc, curr, idx, origin) => {
+    const result = await acc.then();
+    console.log("acc", result);
+    console.log("curr", curr);
+    if (typeof curr[1] === "object") {
+      const sub = Object.entries(curr[1]);
+
+      console.log("sub", sub);
+
+      const reduced = await sub
+        .map((s) => [`${curr[0]}/${s[0]}`, s[1]])
+        .reduce(callback, Promise.resolve({}));
+
+      const pack = { ...result, ...reduced };
+
+      console.log("pack", pack);
+      return pack;
+    } else {
+      const pack = { ...result };
+
+      const d = curr[1];
       const fetched = await fetch(d);
       const text = await fetched.text();
 
       const html: string = markdownConverter.makeHtml(text);
 
-      result[k][key] = html;
+      const reg = /\/([^\/]*)$/g;
+      const filename = reg.exec(curr[0])[1];
+      const category = curr[0].replace("/" + filename, "");
+      console.log("fn", filename);
+
+      pack[category] = pack[category] || {};
+
+      pack[category][filename] = html;
+
+      console.log("pack", pack);
+
+      return Promise.resolve(pack);
     }
-  }
+  };
+
+  const entries = Object.entries(ArticleList);
+
+  console.log("entries", entries);
+  const result = await entries.reduce(callback, Promise.resolve({}));
 
   console.log(result);
 
